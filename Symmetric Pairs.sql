e3fec4226fce804a0865bd3827ccc349f74dcfdc
Shubham Shatabdi <76531082+Ajshubham@users.noreply.github.com>
2024-02-09 12:11:09 +0530

    Create Symmetric Pairs.sql

---
+++ Symmetric Pairs.sql
@@ -0,0 +1,13 @@
+-- You are given a table, Functions, containing two columns: X and Y.
+
+-- Two pairs (X1, Y1) and (X2, Y2) are said to be symmetric pairs if X1 = Y2 and X2 = Y1.
+
+-- Write a query to output all such symmetric pairs in ascending order by the value of X. List the rows such that X1 ≤ Y1. 
+
+SELECT DISTINCT f1.x, f1.y
+FROM Functions AS f1
+JOIN Functions AS f2 ON f1.x = f2.y AND f2.x = f1.y
+WHERE f1.x <= f1.y
+GROUP BY f1.x, f1.y
+HAVING f1.x != f1.y or COUNT(CONCAT(f1.X, f1.Y)) > 1
+ORDER BY f1.x;
