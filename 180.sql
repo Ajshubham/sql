cb6f445ca9d2e8d23d89c2f059043f6386de938d
Shubham Shatabdi <76531082+Ajshubham@users.noreply.github.com>
2024-03-07 13:27:20 +0530

    Create 180.sql

---
+++ 180.sql
@@ -0,0 +1,20 @@
+-- Table: Logs
+
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | id          | int     |
+-- | num         | varchar |
+-- +-------------+---------+
+-- In SQL, id is the primary key for this table.
+-- id is an autoincrement column.
+
+-- Find all numbers that appear at least three times consecutively.
+-- Return the result table in any order.
+
+select distinct l1.num as consecutivenums
+from logs l1, logs l2, logs l3
+where l1.id = l2.id - 1 and
+l2.id = l3.id - 1 and
+l1.num = l2.num and
+l2.num = l3.num;
