b0c556dc402f6834141f30efd432350e08a2b192
Shubham Shatabdi <76531082+Ajshubham@users.noreply.github.com>
2024-03-18 19:52:58 +0530

    Create 1341.sql

---
+++ 1341.sql
@@ -0,0 +1,52 @@
+-- Table: Movies
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | movie_id      | int     |
+-- | title         | varchar |
+-- +---------------+---------+
+-- movie_id is the primary key (column with unique values) for this table.
+-- title is the name of the movie.
+ 
+
+-- Table: Users
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | user_id       | int     |
+-- | name          | varchar |
+-- +---------------+---------+
+-- user_id is the primary key (column with unique values) for this table.
+ 
+-- Table: MovieRating
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | movie_id      | int     |
+-- | user_id       | int     |
+-- | rating        | int     |
+-- | created_at    | date    |
+-- +---------------+---------+
+-- (movie_id, user_id) is the primary key (column with unique values) for this table.
+-- This table contains the rating of a movie by a user in their review.
+-- created_at is the user's review date. 
+ 
+-- Write a solution to:
+-- Find the name of the user who has rated the greatest number of movies. In case of a tie, return the lexicographically smaller user name.
+-- Find the movie name with the highest average rating in February 2020. In case of a tie, return the lexicographically smaller movie name.
+
+(select name as results
+from movierating join users using(user_id)
+group by name
+order by count(*) desc, name
+limit 1)
+union all
+(select title as results
+from movierating join movies using(movie_id)
+where extract(year_month from created_at) = 202002
+group by title
+order by avg(rating) desc, title
+limit 1)
