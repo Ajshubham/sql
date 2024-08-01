8444b01db448281ef9322a734457f27e80ff0a4c
Shubham Shatabdi <76531082+Ajshubham@users.noreply.github.com>
2024-08-01 10:20:37 +0530

    Update 180.sql

--- 180.sql
+++ 180.sql
@@ -12,9 +12,8 @@
 -- Find all numbers that appear at least three times consecutively.
 -- Return the result table in any order.
 
-select distinct l1.num as consecutivenums
-from logs l1, logs l2, logs l3
-where l1.id = l2.id - 1 and
-l2.id = l3.id - 1 and
-l1.num = l2.num and
-l2.num = l3.num;
+SELECT DISTINCT l1.num AS ConsecutiveNums
+FROM Logs l1
+JOIN Logs l2 ON l1.id = l2.id - 1
+JOIN Logs l3 ON l1.id = l3.id - 2
+WHERE l1.num = l2.num AND l2.num = l3.num;
